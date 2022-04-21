03e55ce9e9faca24b2baccc21c8771dbdff50b1b
Prateekpai <prateekvasanthpai@gmail.com>
2022-04-21 21:51:32 +0530

    Welcome to Indian State Census Analyser

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace IndianStateCensusAnalyser
+{
+    public class Analyzer
+    {
+        public static void Main(
+            {
+            Console.WriteLine("Welcome To Indian State Census Analyser Problem");
+        }
+    }
+}
